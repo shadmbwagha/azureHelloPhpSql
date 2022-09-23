33fc6ddacc38e5207cf99a674bd361af2dbcd00f
shad mbwagha <105544140+shadmbwagha@users.noreply.github.com>
2022-09-23 20:20:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php echo "hello Azure By shad";?>
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:mbungula.database.windows.net,1433; Database = mbungulaweb", "shad", "#sylvester777");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "shad", "pwd" => "#sylvester777", "Database" => "mbungulaweb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:mbungula.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+$conn = mysqli_connect("sqlsrv:server = tcp:mbungula.database.windows.net,1433","shad","#sylvester777","mbungulaweb");
 
 $sql = "SELECT * FROM names";
 $result = mysqli_query($conn,$sql);

de9b6d8efef79532b05e435d1c49ae06f7f28ca5
shad mbwagha <105544140+shadmbwagha@users.noreply.github.com>
2022-09-24 11:14:25 +0300

    update connection string

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php echo "hello Azure By shad";?>
 <?php
-$conn = mysqli_connect("mbungula.database.windows.net","shad","#sylvester777","mbungulaweb");
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:mbungula.database.windows.net,1433; Database = mbungulaweb", "shad", "#sylvester777");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-$sql = "SELECT * FROM names";
-$result = mysqli_query($conn,$sql);
-$names = mysqli_fetch_all($result, MYSQLI_ASSOC);
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "shad", "pwd" => "#sylvester777", "Database" => "mbungulaweb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:mbungula.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
+
+$stmt = $conn->query(SELECT * FROM names");
+while($row = $stmt->fetch()){
+  echo $row['first_name']."<br>/";
+}
 print_r($names);
 ?>
 

85a0ae82057ad5f177f169bbec0e06d882615c83
shad mbwagha <105544140+shadmbwagha@users.noreply.github.com>
2022-09-24 11:32:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php echo "hello Azure By shad";?>
 <?php
-<?php
 // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:mbungula.database.windows.net,1433; Database = mbungulaweb", "shad", "#sylvester777");

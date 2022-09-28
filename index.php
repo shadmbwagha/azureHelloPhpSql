069dd83ab0046c69e3389d3e165c9b0cdabc30d2
shad mbwagha <105544140+shadmbwagha@users.noreply.github.com>
2022-09-28 18:37:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
-<?php echo "hello Azure By shad";
-    
+<?php 
+    echo "hello Azure By shad";
+    $conn = mysqli_connect("tcp:mbungula.database.windows.net", "shad", "#sylvester777", "mbungulaweb");
+    if(!$conn){
+        echo "Connection Error".mysqli_connect_error();
+    }
 ?>

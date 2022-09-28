ff4797b535ff275c200526cac642dd1aef738425
shad mbwagha <105544140+shadmbwagha@users.noreply.github.com>
2022-09-28 18:44:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php 
     echo "hello Azure By shad";
-    $conn = mysqli_connect("tcp:mbungula.database.windows.net", "shad", "#sylvester777", "mbungulaweb");
-    if(!$conn){
-        echo "Connection Error".mysqli_connect_error();
-    }
+//     $conn = mysqli_connect("tcp:mbungula.database.windows.net", "shad", "#sylvester777", "mbungulaweb");
+//     if(!$conn){
+//         echo "Connection Error".mysqli_connect_error();
+//     }
+
+
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:mbungula.database.windows.net,1433; Database = mbungulaweb", "shad", "#sylvester777");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "shad", "pwd" => "#sylvester777", "Database" => "mbungulaweb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:mbungula.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 ?>

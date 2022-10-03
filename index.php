a5bd42055e2913111b284a77ff4e29ddc609b917
shad mbwagha <105544140+shadmbwagha@users.noreply.github.com>
2022-10-03 11:21:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,39 @@
 <?php 
-    echo "hello Azure By shad<br>";
-    $conn = mysqli_connect("mbungula.database.windows.net", "shad", "#sylvester777", "mbungulaweb");
-     if(!$conn){
-        echo "Connection Error".mysqli_connect_error();
-    }
+//     echo "hello Azure By shad<br>";
+//     $conn = mysqli_connect("mbungula.database.windows.net", "shad", "#sylvester777", "mbungulaweb");
+//      if(!$conn){
+//         echo "Connection Error".mysqli_connect_error();
+//     }
 
-	$sql = 'SELECT * FROM [dbo].[names]';
-	$res = mysqli_query($conn,$sql);
-	$row = mysqli_fetch_all($res,MYSQLI_ASSOC);
-	print_r($row);
+// 	$sql = 'SELECT * FROM [dbo].[names]';
+// 	$res = mysqli_query($conn,$sql);
+// 	$row = mysqli_fetch_all($res,MYSQLI_ASSOC);
+// 	print_r($row);
 
-
-
-// // PHP Data Objects(PDO) Sample Code:
-// try {
-//     $conn = new PDO("sqlsrv:server = tcp:mbungula.database.windows.net,1433; Database = mbungulaweb", "shad", "#sylvester777");
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// }
-// catch (PDOException $e) {
-//     print("Error connecting to SQL Server.");
-//     die(print_r($e));
-// }
+// PHP Data Objects(PDO) Sample Code:
+ try {
+     $conn = new PDO("sqlsrv:server = tcp:mbungula.database.windows.net,1433; Database = mbungulaweb", "shad", "#sylvester777");
+     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ }
+ catch (PDOException $e) {
+     print("Error connecting to SQL Server.");
+     die(print_r($e));
+ }
 
 
 
 
-// $sql = 'SELECT * FROM [dbo].[names]';
+ $sql = 'SELECT * FROM [dbo].[names]';
+ $statement = $conn->query($sql);
 
-// $statement = $conn->query($sql);
-
-// // get all publishers
-// $publishers = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-// if ($publishers) {
-// 	// show the publishers
-// 	foreach ($publishers as $publisher) {
-// 		echo $publisher['first_name'] . '<br>';
-// 	}
-// }
+ // get all publishers
+ $publishers = $statement->fetchAll(PDO::FETCH_ASSOC);
+ if ($publishers) {
+ 	// show the publishers
+ foreach ($publishers as $publisher) {
+ echo $publisher['first_name'] . '<br>';
+ 	}
+ }
 
 echo "hi";
 

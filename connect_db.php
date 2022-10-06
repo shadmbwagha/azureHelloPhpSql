42dd22ba0eed59eebcb5de8c2739323ea2e9a916
shad mbwagha <105544140+shadmbwagha@users.noreply.github.com>
2022-10-06 16:14:06 +0300

    Update connect_db.php

--- connect_db.php
+++ connect_db.php
@@ -2,7 +2,7 @@
 	
 	// PHP Data Objects(PDO) Sample Code:
 	try {
-    		$conn = new PDO("sqlsrv:server = tcp:mbungula.database.windows.net,1433; Database = mbungulaweb", "shad", "{your_password_here}");
+    		$conn = new PDO("sqlsrv:server = tcp:mbungula.database.windows.net,1433; Database = mbungulaweb", "shad", "#sylvester777");
     		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch (PDOException $e) {
